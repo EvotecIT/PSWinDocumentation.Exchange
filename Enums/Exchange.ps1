71b9e151535dc209c83cf38cf5ed7b43a9041dd7
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-04-30 12:10:41 +0200

    Update

---
+++ Enums/Exchange.ps1
@@ -0,0 +1,20 @@
+﻿Add-Type -TypeDefinition @"
+    using System;
+
+    namespace PSWinDocumentation
+    {
+        [Flags]
+        public enum Exchange {
+            // Clean On-Premises Exchange Data
+            ExchangeUServers,
+            ExchangeUDatabases,
+            ExchangeUPublicFolderDatabase,
+            ExchangeUMailboxes,
+
+            // Prepared On-Premises Exchange Data
+            ExchangeServers,
+            ExchangeDatabasesBackup,
+            ExchangeMailboxesStatistics
+        }
+    }
+"@
